9d1567b4bb17ddb529505197a551c63a4eab38fb
JulienFauvel <jul.fauv@gmail.com>
2015-01-26 20:04:36 +0100

    Formatage

--- src/gameoflife/Cell.java
+++ src/gameoflife/Cell.java
@@ -1,26 +1,40 @@
 package gameoflife;
 
-
+/**
+ * Cellule pouvant etre soit vivante ou non
+ */
 public class Cell {
 	
 	private boolean isAlive;
+	
+	//Nombre de voisins autours de la cellule
 	private int nbVoisin;
 
-	/**
-	 * Cellule pouvant etre soit vivante ou non
-	 */
-	public Cell() {
 
+	public Cell() {
 		isAlive = false;
 		nbVoisin = 0;
-
 	}
 
-
-	public boolean isAlive () { return isAlive;  }
-	public int     getNbVoisin () { return this.nbVoisin; }
-	public void    resetVoisin() { nbVoisin = 0; }
-	public void    addVoisin  () { nbVoisin++;   }
-	public void    setAlive( boolean b ) { isAlive = b; }
+	
+	public boolean isAlive () { 
+		return isAlive;
+	}
+	
+	public int getNbVoisin () {
+		return this.nbVoisin;
+	}
+	
+	public void resetVoisin() {
+		nbVoisin = 0;
+	}
+	
+	public void addVoisin  () { 
+		nbVoisin++;
+	}
+	
+	public void setAlive( boolean b ) {
+		isAlive = b;
+	}
 
 }
